9e93321096fcaad25a284db4b270d1a13eac961b
P4423 <alexzhang@hk.chinamobile.com>
2021-09-23 18:14:13 +0800

    ch1 初识GO

--- src/ch1/main/hello_world.go
+++ src/ch1/main/hello_world.go
@@ -1,8 +1,16 @@
 package main //包，表明代码所在模块（包）
 
-import "fmt" //引入代码依赖
+import (
+	"fmt" //引入代码依赖
+	"os"
+)
 
 //功能实现
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+		fmt.Println("Hello World!", os.Args[1])
+		os.Exit(0)
+	}
+	fmt.Println("Hello Alex!")
 }
